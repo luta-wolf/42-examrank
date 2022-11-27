ba2112b4098c64d875a719e94e4e4081e91eb9f6
Ethelyn Interdictor <einterdi@pr-j4.kzn.21-school.ru>
2022-11-27 14:36:59 +0300

    add new version examrank06

--- ExamRank_06/mini_serv2.c
+++ ExamRank_06/mini_serv2.c
@@ -5,99 +5,100 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-const int BUF_SIZE = 4096 * 42;
-int clients[1024];
-int max_fd, id = -1;
-fd_set ready_read, ready_write, active;
-char buf_read[BUF_SIZE], buf_write[BUF_SIZE], msg[BUF_SIZE];
 
-void f_error(char *str){
-	write(2, str, strlen(str));
-	exit(1);
+const int BUF_SIZE = 42 * 4096;
+typedef struct client {
+    int id;
+    char msg[110000];
+} t_client;
+
+t_client clients[1024];
+
+int max = 0, next_id = 0;
+fd_set active, readyRead, readyWrite;
+char bufRead[BUF_SIZE], bufWrite[BUF_SIZE];
+
+void fatal_error() {
+    write(2, "Fatal error\n", 12);
+    exit(1);
 }
 
-void send_all(int self){
-	for (int i = 3; i <= max_fd; ++i)
-	{
-		if (FD_ISSET(i, &ready_write) && i != self)
-			send(i, buf_write, strlen(buf_write), 0);
-	}
+void send_all(int es) {
+    for (int i = 0; i <= max; i++)
+        if (FD_ISSET(i, &readyWrite) && i != es)
+            send(i, bufWrite, strlen(bufWrite), 0);
 }
 
-int main(int argc, char *argv[]){
-	int sockfd, connfd, res;
-	socklen_t len;
-	struct sockaddr_in servaddr;
+int main(int ac, char **av) {
+    if (ac != 2) {
+        write(2, "Wrong number of arguments\n", 26);
+        exit(1);
+    }
+    int port = atoi(av[1]);
 
-	if (argc != 2)
-		f_error("Wrong number of arguments\n");
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-		f_error("Fatal error\n");
+    bzero(&clients, sizeof(clients));
+    FD_ZERO(&active);
 
-	max_fd = sockfd;
-	FD_ZERO(&active);
-	FD_SET(max_fd, &active);
-	bzero(&clients, sizeof(clients));
+    int serverSock = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSock < 0)
+        fatal_error();
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	servaddr.sin_port = htons(atoi(argv[1]));
+    max = serverSock;
+    FD_SET(serverSock, &active);
 
-	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
-		f_error("Fatal error\n");
-	if (listen(sockfd, 128) != 0)
-		f_error("Fatal error\n");
+    struct sockaddr_in addr;
+    socklen_t addr_len = sizeof(addr);
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(2130706433);
+    addr.sin_port = htons(port);
 
-	while (1)
-	{
-		ready_read = ready_write = active;
+    if ((bind(serverSock, (const struct sockaddr *)&addr, sizeof(addr))) < 0)
+        fatal_error();
+    if (listen(serverSock, 128) < 0)
+        fatal_error();
 
-		if (select(max_fd + 1, &ready_read, &ready_write, NULL, NULL) < 0)
-			continue;
+    while (1) {
+        readyRead = readyWrite = active;
+        if (select(max + 1, &readyRead, &readyWrite, NULL, NULL) < 0)
+            continue ;
 
-		for (int fd_i = 3; fd_i <= max_fd; ++fd_i)
-		{
-			if (FD_ISSET(fd_i, &ready_read) && fd_i == sockfd)
-			{
-				if ((connfd = accept(sockfd, (struct sockaddr *)&servaddr, &len)) < 0)
-					continue;
+        for (int s = 0; s <= max; s++) {
 
-				if (connfd > max_fd)
-					max_fd = connfd;
-				clients[connfd] = ++id;
-				sprintf(buf_write, "server: client %d just arrived\n", clients[connfd]);
-				send_all(connfd);
-				FD_SET(connfd, &active);
-				break;
-			}
-			else if (FD_ISSET(fd_i, &ready_read) && fd_i != sockfd)
-			{
-				if ((res = recv(fd_i, buf_read, BUF_SIZE, 0)) <= 0)
-				{
-					sprintf(buf_write, "server: client %d just left\n", clients[fd_i]);
-					send_all(fd_i);
-					FD_CLR(fd_i, &active);
-					close(fd_i);
-					break;
-				}
-				else
-				{
-					for (int i = 0, j = 0; i < res; ++i, ++j)
-					{
-						msg[j] = buf_read[i];
-						if (msg[j] == '\n')
-						{
-							msg[j] = '\0';
-							sprintf(buf_write, "client %d: %s\n", clients[fd_i], msg);
-							send_all(fd_i);
-							bzero(&msg, sizeof(msg));
-							j = -1;
-						}
-					}
-					break;
-				}
-			}
-		}
-	}
+            if (FD_ISSET(s, &readyRead) && s == serverSock) {
+                int clientSock = accept(serverSock, (struct sockaddr *)&addr, &addr_len);
+                if (clientSock < 0)
+                    continue ;
+                max = (clientSock > max) ? clientSock : max;
+                clients[clientSock].id = next_id++;
+                FD_SET(clientSock, &active);
+                sprintf(bufWrite, "server: client %d just arrived\n", clients[clientSock].id);
+                send_all(clientSock);
+                break ;
+            }
+
+            if (FD_ISSET(s, &readyRead) && s != serverSock) {
+                int res = recv(s, bufRead, 42*4096, 0);
+                if (res <= 0) {
+                    sprintf(bufWrite, "server: client %d just left\n", clients[s].id);
+                    send_all(s);
+                    FD_CLR(s, &active);
+                    close(s);
+                    break ;
+                }
+                else {
+                    for (int i = 0, j = strlen(clients[s].msg); i < res; i++, j++) {
+                        clients[s].msg[j] = bufRead[i];
+                        if (clients[s].msg[j] == '\n') {
+                            clients[s].msg[j] = '\0';
+                            sprintf(bufWrite, "client %d: %s\n", clients[s].id, clients[s].msg);
+                            send_all(s);
+                            bzero(&clients[s].msg, strlen(clients[s].msg));
+                            j = -1;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
 }
